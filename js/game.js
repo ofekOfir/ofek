039953447c947685bc2768812759d91282d91f2f
ofekOfir <ofekofir2001@gmail.com>
2022-11-23 10:36:05 +0200

    fixing the login

---
+++ js/game.js
@@ -0,0 +1,31 @@
+
+// const user = document.querySelector('#username2');
+// const pass = document.querySelector('#password2');
+
+
+
+// for (let i = 0; i < localStorage.length; i++) {
+//     let obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
+//     if (obj.name == username.value && obj.password == password.value) {
+//         sum++;
+//     }
+// }
+// function rememberMe(){
+//     const remember = document.querySelector('#remember');
+//     const username2 = document.querySelector('#username2');
+//     const password2 = document.querySelector('#password2');
+//     if(remember.checked==1){
+//         sessionStorage.setItem('logUser',username2.value);
+//         sessionStorage.setItem('logPass',password2.value);
+//         console.log()
+//     }
+//     else{
+//         sessionStorage.setItem('logUser','');
+//         sessionStorage.setItem('logPass','');
+//     }
+// }
+// rememberMe();
+// user.value = sessionStorage.getItem('logUser');
+// console.log(user.value);
+// pass.value = sessionStorage.getItem('logPass');
+// console.log(pass.value);
--- js/login.js
+++ js/login.js
@@ -1,12 +1,17 @@
-
+const user = document.querySelector('#username2');
+const pass = document.querySelector('#password2');
 const signing = document.querySelector('#signing');
 const register = document.querySelector('#register');
 const login = document.querySelector('#login');
+const loginF = document.querySelector('#failLog');
+const remember = document.querySelector('#remember');
 localStorage.setItem('usernumber', 0);
 register.addEventListener('click', addingUser);
 signing.addEventListener('click', tosign);
 login.addEventListener('mouseover', gamingLobby);
-
+loginF.addEventListener('mouseover', gamingLobby);
+loginF.addEventListener('click', ()=>{alert("User does not exist")});
+// remember.addEventListener('click', rememberMe)
 
 function addingUser() {
     const username2 = document.querySelector('#username2');
@@ -15,7 +20,7 @@ function addingUser() {
     let str = JSON.stringify(obj);
     let b = localStorage.getItem('usernumber');
     let usernumber = parseInt(b) + 1;
-    let u = `ofek${usernumber}`;
+    let u = `monster${usernumber}`;
     localStorage.setItem('usernumber', `${usernumber}`);
     localStorage.setItem(u, str);
     console.log(localStorage);
@@ -35,9 +40,10 @@ function tosign() {
 }
 
 function gamingLobby() {
-    const loginForm = document.querySelector('#loginForm');
     const username = document.querySelector('#username');
     const password = document.querySelector('#password');
+    const buttonLog = document.querySelector('#login');
+    const buttonStay = document.querySelector('#failLog');
     let sum = 0;
     for (let i = 0; i < localStorage.length; i++) {
         let obj = JSON.parse(localStorage.getItem(localStorage.key(i)));
@@ -45,12 +51,37 @@ function gamingLobby() {
             sum++;
         }
     }
+    console.log(sum)
     if(sum==0){
-        loginForm.action="#";
+        buttonLog.style.display="none";
+        buttonStay.style.display="block";
     } 
     else{
-        
-        loginForm.action="/html/game.html";
+        buttonLog.style.display="block";
+        buttonStay.style.display="none";
     }
 
+}
+
+function rememberMe(){
+    const remember = document.querySelector('#remember');
+    const username2 = document.querySelector('#username2');
+    const password2 = document.querySelector('#password2');
+    if(remember.checked==1){
+        sessionStorage.setItem('logUser',username2.value);
+        sessionStorage.setItem('logPass',password2.value);
+        console.log()
+    }
+    else{
+        sessionStorage.setItem('logUser','');
+        sessionStorage.setItem('logPass','');
+    }
+}
+rememberMe();
+window.addEventListener('load',load)
+function load(){
+user.value = sessionStorage.getItem('logUser');
+console.log(user.value);
+pass.value = sessionStorage.getItem('logPass');
+console.log(pass.value);
 }
